f4fee3c95bc60c14ec46b22fc05685492b4813d1
Andrey Timirov <Andrey_Timirov@epam.com>
2019-06-29 17:47:43 -0500

    commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Server {
+  id: number;
+  name: string;
+  status: string;
+};
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  allowServer: boolean = true;
+  servers: Server[] = [];
+  placeholder: string = 'add server...';
+  serverName: string = '';
+  id: number = 1;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  private onAddServer(): void {
+    if (!this.allowServer) return;
+    const isOnline: boolean = Math.random() > 0.5 ? true : false;
+    const status = isOnline && 'online' || 'offline';
+    const newServer: Server = { id: this.id++, name: this.serverName, status };
+    this.servers.push(newServer);
+    this.serverName = '';
+  }
+  private onRemoveServer(id): void {
+    this.servers = this.servers.filter(server => server.id !== id);
+  }
+
+  private getBgColor(server): string {
+    return server.status === 'online' ? 'lightgreen' : 'orange';
+  }
+
+  private getClass(server): object {
+    const check:boolean = server.status === 'online';
+    const classObj = {
+      'online': check,
+      'offline': !check
+    };
+    return classObj;
+  }
+}
